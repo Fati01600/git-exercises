fecc40e22ad75365ef9600f309057e0e11674d96
Fatima Majid <cph-fs156@cphbusiness.dk>
2024-02-01 13:58:12 +0100

    200 instead of 100

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        for(int numbers = 1; numbers <= 100; numbers ++){
+        for(int numbers = 1; numbers <= 200; numbers ++){
         if(numbers % 3 ==0 && numbers % 5 == 0) {
             System.out.println("FizzBuzz");
         } else if (numbers % 3 == 0) {
