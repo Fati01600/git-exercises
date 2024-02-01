d9e21530cce950d0dc957f182aa3109fbd671f9a
Fatima Majid <cph-fs156@cphbusiness.dk>
2024-02-01 13:34:24 +0100

    Hello

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+        for(int numbers = 1; numbers <= 100; numbers ++){
+        if(numbers % 3 ==0 && numbers % 5 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (numbers % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (numbers % 5 == 0) {
+            System.out.println("Buzz");
+        } else{
+                        System.out.println(numbers);
+                    }
+
+
+                }
+            }
+
+        }
